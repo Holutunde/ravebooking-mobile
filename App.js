06c2d592805aaf3e13660eb022725980c41aec44
Holutunde <Holutunde99@gmail.com>
2023-09-17 20:08:43 +0100

    updated

--- App.js
+++ App.js
@@ -1,21 +1,125 @@
 // App.js
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import MainScreen from "./src/screens/MainScreen";
-import GitScreen from "./src/screens/GitScreen";
+import React, { useState } from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import MainScreen from "./src/screens/MainScreen";
+// import GitScreen from "./src/screens/GitScreen";
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Profile" component={MainScreen} />
+//         <Stack.Screen name="Github" component={GitScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
+
+// export default App;
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+import Button from "./src/screens/Button";
+import ImageViewer from "./src/screens/ImageViewer";
+import CircleButton from "./src/screens/circleButton";
+import IconButton from "./src/screens/iconButton";
+
+const PlaceholderImage = require("./src/assets/background-image.png");
+
+export default function App() {
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [2, 2],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setShowAppOptions(true);
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // we will implement this later
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Profile" component={MainScreen} />
-        <Stack.Screen name="Github" component={GitScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            onPress={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+});
